0863252953c806d8dc35599ff90f03ea19d5faa4
Jothin-kumar <contact@jothin.tech>
2022-03-06 12:38:57 +0530

    Implement logic to detect win.
    Signed-off-by: Jothin-kumar <contact@jothin.tech>

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -9,7 +9,8 @@ tk.Label(root, text="Tic Tac Toe", font=('Ariel', 25)).pack()
 current_chr = "X"
 
 play_area = tk.Frame(root, width=300, height=300, bg='white')
-XO_points = []
+X_points = []
+O_points = []
 class XOPoint:
     def __init__(self, x, y):
         self.x = x
@@ -19,16 +20,76 @@ class XOPoint:
         self.button.grid(row=x, column=y)
 
     def set(self):
+        global current_chr
         if not self.value:
             self.button.configure(text=current_chr, bg='snow', fg='black')
             self.value = current_chr
+            if current_chr == "X":
+                X_points.append(self)
+                current_chr = "O"
+            elif current_chr == "O":
+                O_points.append(self)
+                current_chr = "X"
+        check_win()
 
     def reset(self):
         self.button.configure(text="", bg='white')
+        if self.value == "X":
+            X_points.remove(self)
+        elif self.value == "O":
+            O_points.remove(self)
         self.value = None
-for x in range(3):
-    for y in range(3):
-        XO_points.append(XOPoint(x, y))
+for x in range(1, 4):
+    for y in range(1, 4):
+        XOPoint(x, y)
+class WinningPossibility:
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+    def check(self, for_chr):
+        p1_satisfied = False
+        p2_satisfied = False
+        p3_satisfied = False
+        if for_chr == 'X':
+            for point in X_points:
+                if point.x == self.x1 and point.y == self.y1:
+                    p1_satisfied = True
+                elif point.x == self.x2 and point.y == self.y2:
+                    p2_satisfied = True
+                elif point.x == self.x3 and point.y == self.y3:
+                    p3_satisfied = True
+        elif for_chr == 'O':
+            for point in O_points:
+                if point.x == self.x1 and point.y == self.y1:
+                    p1_satisfied = True
+                elif point.x == self.x2 and point.y == self.y2:
+                    p2_satisfied = True
+                elif point.x == self.x3 and point.y == self.y3:
+                    p3_satisfied = True
+        return all([p1_satisfied, p2_satisfied, p3_satisfied])
+winning_possibilities = [
+    WinningPossibility(1, 1, 1, 2, 1, 3),
+    WinningPossibility(2, 1, 2, 2, 2, 3),
+    WinningPossibility(3, 1, 3, 2, 3, 3),
+    WinningPossibility(1, 1, 2, 1, 3, 1),
+    WinningPossibility(1, 2, 2, 2, 3, 2),
+    WinningPossibility(1, 3, 2, 3, 3, 3),
+    WinningPossibility(1, 1, 2, 2, 3, 3),
+    WinningPossibility(3, 1, 2, 2, 1, 3)
+]
+def check_win():
+    print("Checking win")
+    for possibility in winning_possibilities:
+        if possibility.check('X'):
+            print("X won!")
+            break
+        elif possibility.check('O'):
+            print("O won!")
+            break
 play_area.pack(pady=10, padx=10)
 
 root.mainloop()
